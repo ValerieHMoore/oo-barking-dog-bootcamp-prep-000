b68f8c39a5ece132fa3781bc12a2d02934f29f8e
Valerie Moore <periwinkle17@outlook.com>
2018-05-27 17:54:01 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,16 +1,5 @@
 # Your code goes here!
 class Dog
-  def initialize(name)
-  @name = (name)
+  def bark
+    puts "woof!"
   end
-  	 
-  def name=(name)
-  @name = name
-  end
-
-  def name
-    @name
-  end
-end
-
-
