6aafbc095af7e575324e0d7a61a189afe813d194
Valerie Moore <periwinkle17@outlook.com>
2018-05-27 18:02:39 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,11 +4,16 @@ class Dog
     puts "woof!"
   end
   
-  def name=(dog_name)
-    this_dogs_name = dog_name
-  end
-  	 
+  def name=(dogs_name)
+    @this_dogs_name = dogs_name
+    end
+
   def name
-    this_dogs_name
+  @this_dogs_name
   end
+
+  lassie = Dog.new
+  lassie.name = "Lassie"
+  puts lassie.name
+
 end
