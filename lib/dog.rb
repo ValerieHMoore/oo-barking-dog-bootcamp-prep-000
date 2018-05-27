7c3c8cd136ceb9a05d8cb0f1fbb4f141fafb31a2
Valerie Moore <periwinkle17@outlook.com>
2018-05-27 17:50:04 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,15 +1,16 @@
 # Your code goes here!
 class Dog
   def initialize(name)
-    @name = name
+  @name = (name)
   end
-  
+  	 
+  def name=(name)
+  @name = name
+  end
+
   def name
     @name
   end
+end
 
-11.	  def name=(new_name)
-12.	    @name = new_name
-13.	  end
-14.	 
-15.	end
+
